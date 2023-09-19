83cfe0e3cb29c78abb33a127462da11f7eda75f1
Juanma <77707900+JuanmaGuariste@users.noreply.github.com>
2023-09-19 16:43:30 -0300

    cambios

--- PreEntrega3/src/app.js
+++ PreEntrega3/src/app.js
@@ -38,32 +38,6 @@ app.use(passport.initialize());
 
 mongoose.connect(environment.MONGO_URL);
 
-// const swaggerOptions = {
-// 	definition: {
-// 		openapi: '3.0.1',
-// 		info: {
-// 			title: 'Upsoon API',
-// 			version: '1.0.0',
-// 			description: 'Upsoon API Information',
-// 		},
-// 		servers: [
-// 			{
-// 				url: 'http://localhost:8080',
-// 			},
-// 			{
-// 				url: 'http://api-uat.upsoon.com',
-// 			},
-// 			{
-// 				url: 'http://api-preprod.upsoon.com',
-// 			},
-// 			{
-// 				url: 'http://api.upsoon.com',
-// 			},
-// 		]
-// 	},
-// 	apis: ['./src/routers/*.js'],	
-// }
-
 const spects = swaggerJsDoc(swaggerOptions);
 
 app.use(
